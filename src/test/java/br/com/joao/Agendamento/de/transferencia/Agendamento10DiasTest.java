537eb63352a33a9ae009578d89688e1d29352036
Joao Felipe <joao.yuki@gmail.com>
2020-03-15 16:27:40 -0300

    Adicionado estratégia de calcula de taxa para agendamentos até 10 dias Adicionado testes unitários

---
+++ src/test/java/br/com/joao/Agendamento/de/transferencia/Agendamento10DiasTest.java
@@ -0,0 +1,70 @@
+package br.com.joao.Agendamento.de.transferencia;
+
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import br.com.joao.AgendamentoTransferencia.Agendamento10Dias;
+import br.com.joao.AgendamentoTransferencia.request.TransferenciaRequest;
+
+@RunWith(MockitoJUnitRunner.class)
+public class Agendamento10DiasTest {
+
+	Agendamento10Dias agendamento12Dias;
+	
+	@Before
+	public void init() {
+		
+		agendamento12Dias = new Agendamento10Dias();
+		
+	}
+	
+	@Test
+	public void teste() {
+		assertTrue(1 < 2);
+	}
+	
+	@Test
+	public void deveriaCalcularTaxaCom2Dias() {
+		
+		TransferenciaRequest request = new TransferenciaRequest(123, 234, 10.0, LocalDate.now().plusDays(2));
+		double calcularTransferencia = agendamento12Dias.calcularTransferencia(request);
+		assertEquals(new Double(32), calcularTransferencia);
+		
+	}
+	
+	@Test
+	public void deveriaCalcularTaxaCom10Dias() {
+		
+		TransferenciaRequest request = new TransferenciaRequest(123, 234, 10.0, LocalDate.now().plusDays(10));
+		double calcularTransferencia = agendamento12Dias.calcularTransferencia(request);
+		assertEquals(new Double(112), calcularTransferencia);
+		
+	}	
+	
+	@Test
+	public void deveriaCalcularTaxaCom11Dias() {
+		
+		TransferenciaRequest request = new TransferenciaRequest(123, 234, 10.0, LocalDate.now().plusDays(11));
+		double calcularTransferencia = agendamento12Dias.calcularTransferencia(request);
+		assertEquals(new Double(0), calcularTransferencia);
+		
+	}	
+	
+	@Test
+	public void deveriaCalcularTaxaCom0Dias() {
+		
+		TransferenciaRequest request = new TransferenciaRequest(123, 234, 10.0, LocalDate.now());
+		double calcularTransferencia = agendamento12Dias.calcularTransferencia(request);
+		assertEquals(new Double(0), calcularTransferencia);
+		
+	}		
+	
+}
