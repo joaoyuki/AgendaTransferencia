993984e74ab59cdb2231b822ed34d4aa392daf3d
João Felipe <joao.yuki@gmail.com>
2020-03-15 11:49:56 -0300

    Criando o projeto

---
+++ src/main/java/br/com/joao/Agendamento/de/transferencia/AgendamentoDeTransferenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.joao.Agendamento.de.transferencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoDeTransferenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoDeTransferenciaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/joao/Agendamento/de/transferencia/AgendamentoDeTransferenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.Agendamento.de.transferencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoDeTransferenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
